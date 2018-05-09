0fc609373aee19e1d3b74a3141bec94500fac66f
Kyle Andrews <kylemandrews@gmail.com>
2018-05-09 16:13:22 -0400

    Organize Box: Fixed Header

--- src/App.js
+++ src/App.js
@@ -6,32 +6,32 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-      <div className="header">
-        <h2 className="title">Stay up to date with ecommerce trends with Shopify\s newsletter</h2>
-        <p className="dash">__</p>
-      </div>
-        <div className="form">
-          <h3 className="free-tips">Subscribe for free marketing tips</h3>
-          <span>
-            <div>
-            <input/>
-            <p className="email-warning">Please enter valid email address</p>
+        <div className="box">
+          <div className="header">
+            <h2 className="title">Stay up to date with ecommerce trends  with Shopify's Newsletter</h2>
+            <p className="dash">__</p>
+          </div>
+
+          <form>
+            <p className="free-tips">Subscribe for free marketing tips</p>
+            <span className="inputs">
+              <input className="email-biz" style={{float:"left"}}/>
+              <input className="dropdown" style={{float:"right"}}/>
+            </span>
+              <div>
+                <button>Sign up now</button>
+              </div>
+
+              <div>
+                < button>Submitting...</button>
+              </div>
+          </form>
+
+            <div className="thanks">
+              <h3>Thanks for subscribing</h3>
+              <p>You--ll start receiving free tips and resources soon.</p>
             </div>
-          </span>
-          <span>
-            <input/>
-          </span>
-          <div>
-          < button>Sign up now</button>
           </div>
-          <div>
-          < button>Submitting...</button>
-          </div>
-          <div className="thanks">
-            <h3>Thanks for subscribing</h3>
-            <p>You--ll start receiving free tips and resources soon.</p>
-          </div>
-        </div>
       </div>
     );
   }
