3a1fdc4e7047a2f13670e7ed4ef5d99d9616ee9b
Kyle Andrews <kylemandrews@gmail.com>
2018-05-09 13:36:05 -0400

    Add CSS & HTML Classes

--- src/App.js
+++ src/App.js
@@ -6,26 +6,31 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-
-        <div>
+      <div className="header">
+        <h2 className="title">Stay up to date with ecommerce trends with Shopify\s newsletter</h2>
+        <p className="dash">__</p>
+      </div>
+        <div className="form">
+          <h3 className="free-tips">Subscribe for free marketing tips</h3>
           <span>
             <div>
             <input/>
-            <p>Please enter valid email address</p>
+            <p className="email-warning">Please enter valid email address</p>
             </div>
           </span>
-
           <span>
             <input/>
           </span>
-          < button className="signup" value="Sign up now">Sign up now</button>
+          <div>
+          < button>Sign up now</button>
+          </div>
+          <div>
+          < button>Submitting...</button>
+          </div>
+          <div className="thanks">
+            <h3>Thanks for subscribing</h3>
+            <p>You--ll start receiving free tips and resources soon.</p>
+          </div>
         </div>
       </div>
     );
