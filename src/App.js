ca8656ef7875606464d9274b56a624ef8c803902
Kyle Andrews <kylemandrews@gmail.com>
2018-05-09 12:21:52 -0400

    Add Inputs & Button

--- src/App.js
+++ src/App.js
@@ -13,6 +13,20 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+
+        <div>
+          <span>
+            <div>
+            <input/>
+            <p>Please enter valid email address</p>
+            </div>
+          </span>
+
+          <span>
+            <input/>
+          </span>
+          < button className="signup" value="Sign up now">Sign up now</button>
+        </div>
       </div>
     );
   }
